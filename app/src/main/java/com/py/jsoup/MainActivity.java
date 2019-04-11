c9b3024c402bd8f72311f3cc294dd4f1d95170c6
pankaj443 <pankajofficial443@gmail.com>
2019-04-11 15:18:17 +0530

    Question list added

--- app/src/main/java/com/py/jsoup/MainActivity.java
+++ app/src/main/java/com/py/jsoup/MainActivity.java
@@ -5,8 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.jsoup.Jsoup;
@@ -17,12 +19,16 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import static android.R.layout.simple_list_item_1;
+
 public class MainActivity extends AppCompatActivity {
 
         TextView textView;
         EditText editText;
         Button button;
-
+        ListView lv;
+    ArrayList<String> questions = new ArrayList<>();
+    ArrayAdapter<String> arrayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
         textView = (TextView) findViewById(R.id.textView);
         editText = (EditText) findViewById(R.id.editText);
+        lv = (ListView) findViewById(R.id.dynamic);
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,10 +53,10 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected String doInBackground(Void... params) {
             String title ="";
-            Document doc;
+            Document doc,document;
             String username = String.valueOf(editText.getText());
             username = username.replaceAll("\\s+","");
-            ArrayList<String> questions = new ArrayList<>();
+
 
             try {
                 int a = 0,b=0,c=0,d=0,k=0,f=0;
@@ -65,45 +72,55 @@ public class MainActivity extends AppCompatActivity {
                 for (int count = 1; count <= Integer.parseInt(pages); count++)
                 { doc = Jsoup.connect("https://codeforces.com/submissions/"+username+"/page/" + count).get();
 
-                for (Element table : doc.select(".status-frame-datatable")) {
-                    for (Element row : table.select("tr:contains(Accepted)")) {
+                for (Element table : doc.select(".status-frame-datatable"))
+                    //String number = row.text().substring(0, 7);
+                    //questions.add(number);
+                    for (Element row : table.select("tr:contains(Accepted)"))
+                        for (Element element : row.select("[href*=/contest/]")) {
+                            Log.i("MSGNM", String.valueOf(element.text()));
+
+                           if (element.text().substring(0,1).equals("A")||element.text().substring(0,1).equals("B")||element.text().substring(0,1).equals("C")||element.text().substring(0,1).equals("D")||element.text().substring(0,1).equals("E")||element.text().substring(0,1).equals("F")){
+                               questions.add(element.text());
+                           }
+
+                            String level = element.text().substring(0,1);
+
+                            level = level.replace("#", "");
+                            level = level.replaceAll("\\s+", "");
+                            Log.i("MSGNM", level);
+
+
+                            switch (level) {
+                                case "A":
+                                    a++;
+                                    break;
+                                case "B":
+                                    b++;
+                                    break;
+                                case "C":
+                                    c++;
+                                    break;
+                                case "D":
+                                    d++;
+                                    break;
+                                case "E":
+                                    k++;
+                                    break;
+                                case "F":
+                                    f++;
+                                    break;
 
 
 
-
-                        String number = row.text().substring(0,7);
-                        questions.add(number);
-
-
-                        String level = row.text().substring(27+username.length(),30+username.length());
-
-                       level=level.replace("#","");
-                        level = level.replaceAll("\\s+","");
-                        Log.i("MSGNM",level);
-
-
-                        switch (level)
-                        {
-                            case "A":a++;break;
-                            case "B":b++;break;
-                            case "C":c++;break;
-                            case "D":d++;break;
-                            case "E":k++;break;
-                            case "F":f++;break;
-
-
-
+                            }
                         }
-
-
-
-                         }
-                    }
 
                  }
 
-                 title += "A ::"+ a +"\nB ::" +b+ "\nC::"+ c +"\nD ::" +d+ "\nE ::"+ k +"\nF ::" +f;
-                Log.i("MSGNM",title);
+                title += "A ::"+ a +"\nB ::" +b+ "\nC::"+ c +"\nD ::" +d+ "\nE ::"+ k +"\nF ::" +f;
+
+
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -115,6 +132,12 @@ public class MainActivity extends AppCompatActivity {
         protected void onPostExecute(String result) {
             //if you had a ui element, you could display the title
             ((TextView)findViewById (R.id.textView)).setText (result);
+            arrayAdapter = new ArrayAdapter<String>(
+                    getApplicationContext(),
+                    simple_list_item_1,
+                    questions);
+
+            lv.setAdapter(arrayAdapter);
         }
     }
 }
